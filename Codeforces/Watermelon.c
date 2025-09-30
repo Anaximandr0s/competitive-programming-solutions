14094836ffb1d9ae64c0f971166aaecfff363b72
Mohamed Doukkani <mdoukkani8@gmail.com>
2025-09-30 20:21:29 +0100

    Solving first problemset at codeforces : watermelon

--- Codeforces/Watermelon.c
+++ Codeforces/Watermelon.c
@@ -1,12 +1,15 @@
+//
+// Created by Mohamed Doukkani on 30/09/2025.
+//
 #include <stdio.h>
 
 int main(void) {
     int w = 0;
     scanf("%d", &w);
-        if (w % 2 == 0 && w > 2) {
-            printf("YES\n");
-        }else {
-            printf("NO\n");
-        }
-        return 0;
+    if (w % 2 == 0 && w > 2) {
+        printf("YES\n");
+    }else {
+        printf("NO\n");
+    }
+    return 0;
 }
