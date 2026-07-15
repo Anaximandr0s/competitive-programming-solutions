45d9182a3623df4c236d9d3eadfb992bed5e05c3
Mohamed 穆罕默德 <70285301+Anaximandr0s@users.noreply.github.com>
2026-07-15 12:21:10 +0100

    Time: 0 ms (100%), Space: 19.4 MB (78.94%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -4,13 +4,13 @@ class Solution:
         new_var = 0
         for operation in operations:
             if operation == "+":
-                new_var = int(ans[-1]) + int(ans[-2])
+                new_var = ans[-1] + ans[-2]
                 ans.append(new_var)
             elif operation == "C":
                 ans.pop()
             elif operation == "D":
-                new_var = int(ans[-1]) * 2
+                new_var = ans[-1] * 2
                 ans.append(new_var)
             else:
-                ans.append(operation)
-        return sum(map(int, ans))
+                ans.append(int(operation))
+        return sum(ans)
